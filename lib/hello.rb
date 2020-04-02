04fb05d067167d742dc432f7d43319fdf4678dde
jamelia harrison <jameliaharrison@gmail.com>
2020-04-02 04:08:37 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -8,11 +8,11 @@ def hello_t(array)
 end
 
 # call your method here!
-hello_t(["Tom", "Tim", "Jam"]) do |name|
+hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
 end
 
-# call your method here!
+
 
